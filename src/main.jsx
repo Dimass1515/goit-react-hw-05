ddaae90e3a5343b101a3ea6e6586b33399beecc2
Dimass1515 <di.tsvigun@gmail.com>
2024-06-10 17:56:07 +0300

    last

--- src/main.jsx
+++ src/main.jsx
@@ -1,13 +1,13 @@
-/* eslint-disable react/jsx-no-undef */
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/App/App'
-import './index.css';
-import 'modern-normalize';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
+import App from "./App.jsx";
+import "./index.css";
 
-
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
